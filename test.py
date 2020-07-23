d16c2d830cf4100cd845711e7f08540a71ca33d6
abhimamg <58421652+abhimamg@users.noreply.github.com>
2020-07-23 10:51:24 +0100

    renamed data.csv

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ import numpy as np
 
 
 st.write("""
-# My first Application
+# My first Application. Kya baat hai
 """)
 
 st.sidebar.header('User Input Parameters')
 
 uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
 if uploaded_file is None:
-    data = pd.read_csv("dat1.csv")
+    data = pd.read_csv("data.csv")
 else:
     data = pd.read_csv(uploaded_file)
 st.write(data)
