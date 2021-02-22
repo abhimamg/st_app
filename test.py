dee4eda2bb31be20f3ac2b938d830560f1d77803
Abhishek Mamgain <abhi.mamg@gmail.com>
2021-02-22 21:55:33 +0000

    pass

--- test.py
+++ test.py
@@ -3,13 +3,14 @@ import pandas as pd
 import numpy as np
 
 
-st.write("""
+st.write(
+    """
 # Cathodic Protection Design. Khoya khoya chand. KHula aasmaan.
 ## DNVGL-RP-F103
-""")
+"""
+)
 
-st.sidebar.header('User Input Parameters')
-
+st.sidebar.header("User Input Parameters")
 
 
 uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
@@ -18,10 +19,11 @@ if uploaded_file is None:
 else:
     data = pd.read_csv(uploaded_file)
 
-if st.checkbox('Show Input'): st.table(data)
+if st.checkbox("Show Input"):
+    st.table(data)
 
 aa = st.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone'))
+    "How would you like to be contacted?", ("Email", "Home phone", "Mobile phonewa")
+)
 
 st.write(np.random.randn(5, 3))
